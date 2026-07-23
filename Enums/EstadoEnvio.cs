72ebdee1162414f2649c48d94b26b28176971f5b
Jualy <jualyfabre17@gmail.com>
2026-07-23 14:29:15 -0400

    Finalizacion de creacion de enums e interfaces

---
+++ Enums/EstadoEnvio.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogiTrack.Enums
+{
+    public class EstadoEnvio
+    {
+        enum estadoEnvio 
+        { 
+         enEspera = 1,
+         enTransito = 2,
+         entregado = 3,
+         devuelto = 4,
+        }
+
+    }
+}
