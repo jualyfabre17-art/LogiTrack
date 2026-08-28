ef51fafa357861262488c27c647b091b4beb9cbc
Jualy <jualyfabre17@gmail.com>
2026-08-28 07:41:34 -0400

    Creando las acciones de las gestion de conductores

---
+++ GestorConductores/GestorConductores.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using LogiTrack.Conductors;
+
+namespace LogiTrack.GestorConductores
+{
+    public class GestorConductores
+    {
+        private  List<Conductor> conductores;
+
+        public GestorConductores()
+        {
+            conductores = new List<Conductor>();
+        }
+
+        public void Agregar(Conductor conductor)
+        {
+            conductores.Add(conductor);
+        } 
+
+        public void MostrarTodos()
+        {
+            if(!conductores.Any())
+            {
+                Console.WriteLine("No hay conductores enlistados");
+            }
+            else
+            {
+                foreach(var conductor in conductores)
+                {
+                    conductor.MostrarInfo();
+                }
+            }
+        }
+
+        public List<Conductor> ObtenerTodos()
+        {
+            return conductores;
+        }
+    }
+}
